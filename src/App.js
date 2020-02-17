78b5d259348def6440999b6763b83b292b842975
Leo <leowei727@yahoo.ca>
2020-02-17 14:00:34 -0700

    updated our menu items to navigate to with ROouterr

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
-import Homepage from './pages/homepage/homepage.component'
+import Homepage from './pages/homepage/homepage.component';
+import {Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path ='/' component={Homepage}/>
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
